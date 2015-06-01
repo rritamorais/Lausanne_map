e5e2ded9bd2701af2866b45412394fd80acf71dd
Rita Morais <rita@tasboa.com>
2015-06-01 13:31:18 +0200

    Added foursquare
    - tweaked wikilinks

--- js/app.js
+++ js/app.js
@@ -1,120 +1,39 @@
 //MODEL
-var $wikiElem = $('#wikipedia-links');
+var $wikiElem = $('#wiki');
+var $fourElem = $('#four');
 
 var initialLocations = [
   {
       name : 'Parc de Mon Repos',
       lat : 46.518889,
       lng : 6.642778,
-      description: 'The Parc de Mon Repos is a public park of the city of Lausanne, Switzerland.',
-      tags: ["restaurants", "bars"]
+      tags: ["parks", "nature", "gardens"]
 
   },
   {
       name : 'Cantonal Museum of Fine Arts',
       lat : 46.5239,
       lng : 6.6341,
-      description: 'The Cantonal Museum of Fine Arts is an art museum in Lausanne, Switzerland',
-      tags: ["restaurants", "bars"]
+      tags: ["museum", "art", "culture"]
 
   },
   {
       name : 'Le Flon',
       lat : 46.5140495,
       lng : 6.623333,
-      description: 'Le Flon is a district of the city of Lausanne, in Switzerland. It is served by Lausanne Metro lines 1 and 2 from Lausanne-Flon station.',
-      tags: ["parks"]
+      tags: ["shopping", "bars", "clothes"]
   },
   {
       name : 'The Olympic Museum',
       lat : 46.508611,
       lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-    {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-    {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-    {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-    {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-  {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-  {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-  {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-  {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-  {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-  {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
-  },
-  {
-      name : 'The Olympic Museum',
-      lat : 46.508611,
-      lng : 6.633889,
-      description: 'The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.The Olympic Museum in Lausanne, Switzerland houses permanent and temporary exhibits relating to sport and the Olympic movement. With more than 10,000 pieces, the museum is the largest archive of Olympic Games in the world; and one of Lausannes prime tourist site draws; attracting more than 250,000 visitors each year.',
-      tags: ["restaurants", "bars"]
+      tags: ["museum", "olympic", "culture"]
   },
   {
       name : 'Rolex Learning Center',
       lat : 46.518333,
       lng: 6.568056,
-      description: 'The Rolex Learning Center is the campus hub and library for the École polytechnique fédérale de Lausanne, in Lausanne, Switzerland. Designed by the architects SANAA, it opened on February 22nd, 2010.',
-      tags: ["restaurants", "bars"]
+      tags: ["architecture", "lybrary", "culture"]
   }
 ]
 
@@ -129,8 +48,9 @@ var ViewModel = function() {
     this.lat = data.lat;
     this.lng = data.lng;
     this.tags = data.tags;
-    this.description = data.description;
+    this.description = ko.observableArray([]);;
     this.wikiLinks = ko.observableArray([]);
+    this.fourImgs = ko.observableArray([]);
     this.marker = new google.maps.Marker();
 
   }
@@ -150,6 +70,7 @@ var ViewModel = function() {
     self.currentLocation(clickedLoc);
     loadWiki();
     scrollDown();
+    loadFoursquare();
   };
 
   //Scrolls nav down to info div
@@ -190,81 +111,78 @@ var ViewModel = function() {
       }
     }
     return false;
-};
+  };
 
   //creates map
   var map = initialize();
   var markerList = [];
 
-function initialize() {
+  function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(46.5140491, 6.623334),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.SATELLITE
     };
     return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  };
+    };
 
-  //creates markers
+    //creates markers
   function createMarkers() {
-  ko.utils.arrayForEach(ViewModel.filteredItems(), function(locItem){
-    var myLatlng = new google.maps.LatLng(locItem.lat,locItem.lng);
-    var image = 'img/marker.png';
-    marker = new google.maps.Marker({
-      position: myLatlng,
-      icon: image,
-      map: map,
-      animation: google.maps.Animation.DROP,
-      title: locItem.name
-    });
+    ko.utils.arrayForEach(ViewModel.filteredItems(), function(locItem){
+      var myLatlng = new google.maps.LatLng(locItem.lat,locItem.lng);
+      var image = 'img/marker.png';
+      marker = new google.maps.Marker({
+        position: myLatlng,
+        icon: image,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: locItem.name
+      });
 
-    markerList.push(marker);
-    //push the right marker to the location list
-    self.locationList()[markerList.length -1].marker = marker;
+      markerList.push(marker);
+      //push the right marker to the location list
+      self.locationList()[markerList.length -1].marker = marker;
 
-    google.maps.event.addListener(marker, 'click', (function(markerCopy) {
-      return function() {
-        self.currentLocation(self.locationList()[markerList.indexOf(markerCopy)]);
-        loadWiki();
-        scrollDown();
+      google.maps.event.addListener(marker, 'click', (function(markerCopy) {
+        return function() {
+          self.currentLocation(self.locationList()[markerList.indexOf(markerCopy)]);
+          loadWiki();
+          scrollDown();
 
-        self.toggleBounce(markerCopy);
-      };
-    })(marker));
-  });  
-};
-createMarkers();
-
-
-  function clearMarkers() {
-    markerList.forEach(function(item) { 
-      item.setMap(null);
-    });
+          self.toggleBounce(markerCopy);
+        };
+      })(marker));
+    });  
   };
+  createMarkers();
 
+  //sets the markers on map - receives filtered list as it is filtered
   function setMarkers(listLoc) {
     listLoc.forEach(function(item) { 
       item.marker.setMap(map);
     });
   };
 
+  //clears markers from map
+  function clearMarkers() {
+    markerList.forEach(function(item) { 
+      item.setMap(null);
+    });
+  };
 
+  //toggles bounce animation for markers
+  this.toggleBounce = function(clickMarker) {
+    //stops any bouncing before applying new one
+    markerList.forEach(function(item) { 
+      item.setAnimation(null);        
+    });
 
-
-    this.toggleBounce = function(clickMarker) {
-      markerList.forEach(function(item) { 
-        item.setAnimation(null);        
-      });
-
-
-      if (clickMarker.getAnimation() != null) {
-        clickMarker.setAnimation(null);
-      } else {
-        clickMarker.setAnimation(google.maps.Animation.BOUNCE);
-      }
-    };
-
-
+    if (clickMarker.getAnimation() != null) {
+      clickMarker.setAnimation(null);
+    } else {
+      clickMarker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  };
 
 //WIKI
 function loadWiki() {
@@ -283,26 +201,72 @@ function loadWiki() {
     dataType: 'jsonp',
     success: function(response){
       var articleList = response[1];
+      var descriptionList = response[2];
+      var description = descriptionList[0];
 
       self.currentLocation().wikiLinks([]);
+      self.currentLocation().description([]);
+      self.currentLocation().description.push(description);
+
+
 
       $.each(articleList, function(i) {
       var articleTitle = articleList[i];
-      var articleUrl = 'http://en.wikipedia.org/wiki/'+ articleTitle ;
-      self.currentLocation().wikiLinks.push(articleUrl);
+      self.currentLocation().wikiLinks.push(articleTitle);
 
       });
 
       clearTimeout(wikiRequestTimeout);
     }     
 } );
-
     return false;
-
   };
+  
+
+  //FOURSQUARE
+  function loadFoursquare() {
+    var Latlng = self.currentLocation().lat+','+self.currentLocation().lng;
+    $fourElem.text("");
+
+    var selfLoc = self.currentLocation().name;
+
+    var fourUrl = 'https://api.foursquare.com/v2/venues/search?ll='+Latlng+'&client_id=IL5PRAKZOFDNHJSIYQP0RIMSWWWTEJQVBPVWW3FXK4A42WIX&client_secret=EG3NSCRU3BVVWZN0MLK0MUFCJOV02MUSOTSY0ETZ4DITJ5H2&v=20150106';
+
+      var fourRequestTimeout = setTimeout(function(){
+        $fourElem.text("Sorry, we couldn't find any fourthsquare resources");
+      }, 4000);
+
+      $.ajax( {
+      url: fourUrl ,
+      dataType: 'jsonp',
+      success: function(response){
+        console.log(response);
+        var venueId = response.response.venues[0].id;
+        var photoUrl = 'https://api.foursquare.com/v2/venues/'+ venueId + '?oauth_token=D4PTDXIIRZ32ZDZEVRMPOAEQZEX1QSLWDFJKQ0Q4FDG42LPF&v=20150601';
+
+        clearTimeout(fourRequestTimeout);
+
+        $.ajax( {
+        url: photoUrl ,
+        dataType: 'jsonp',
+        success: function(response){
+          var fourimgUrl = response.response.venue.bestPhoto.prefix + '320x200' + response.response.venue.bestPhoto.suffix;
+          //clears array
+          self.currentLocation().fourImgs([]);
+          //pushes results into array
+          self.currentLocation().fourImgs.push(fourimgUrl);
+        
+        clearTimeout(fourRequestTimeout);
+        }     
+        } );
+      }     
+      } );
+      return false;
+
+  }; 
 
 
-};// <-- end of ViewModel
+};//end of ViewModel
 
 var stringStartsWith = function (string, startsWith) {          
         string = string || "";
